07a08cae148de62b8bcb973788a2c30bba3ce495
Mitchell Barry <mitch.barry@gmail.com>
2016-12-07 16:53:06 -0600

    [FEATURE] Drill down to any depth of your model

--- control/lib/PopoverHelper.js
+++ control/lib/PopoverHelper.js
@@ -39,7 +39,7 @@ sap.ui.define([
       return this._aCurrentPropertyPath.length;
     },
 
-    getCurrentPropertyPath: function () {
+    getCurrentPropertyPath: function (bIncludeModelPrefix) {
       var sPath = '';
       var sModelName = this.getCurrentModelName();
       var sModelPathSeparator = '>';
@@ -50,15 +50,19 @@ sap.ui.define([
         sModelName = '';
       }
 
-      sPath += sModelName
-      sPath += sModelPathSeparator;
-      sPath += this._aCurrentPropertyPath.join('/');
+      if (bIncludeModelPrefix) {
+        sPath += sModelName
+        sPath += sModelPathSeparator;
+      }
+      
+      sPath += (this._aCurrentPropertyPath.join('/') || '/');
       return sPath;
     },
 
     goUpperLevel: function (iDepth) {
       if (this._aCurrentPropertyPath.length > 1) {
         this._aCurrentPropertyPath.pop();
+        this._setPropertyTitle(this.getCurrentPropertyPath(true));
       } else {
         $.sap.log.warning('com.mitchbarry.controls.ModelInspector: Already at highest level of property tree');
       }
@@ -100,6 +104,20 @@ sap.ui.define([
         oNavCon.to(oNextPage);
       }
       
+      return this;
+    },
+
+    /**
+     * Set Property Title
+     * 
+     * Typically used to set property path in title area.
+     * 
+     * @param {string} sTitle - title to set on Popover page
+     * @returns {com.mitchbarry.controls.lib.PopoverHelper} this - this object to allow for method chaining
+     */
+    _setPropertyTitle: function (sTitle) {
+      var oPropertiesPage = Fragment.byId(this.getFragmentId(), 'properties');
+      oPropertiesPage.setTitle(sTitle);
       return this;
     },
 
@@ -150,7 +168,7 @@ sap.ui.define([
     },
 
     getPopoverModel: function () {
-      return this.getContext().getModel(this._sPopoverModelName);;
+      return this.getContext().getModel(this._sPopoverModelName);
     },
 
     getModelHelper: function () {
@@ -200,9 +218,35 @@ sap.ui.define([
     onInspect: function (oEvent) {
       var sModelName = oEvent.getSource().data().ModelName;
       this.setCurrentModelName(sModelName);
+      var oPopoverModel = this.getPopoverModel();
+      var oModelHelper = this.getModelHelper();
+      oPopoverModel.setProperty('/InspectingModel', oModelHelper.getModelInfo(sModelName));
       this._loadPropertiesAtDepth();
+      this._setPropertyTitle(this.getCurrentPropertyPath(true));
       this._navigateTo('properties');
       return;
+    },
+
+    onPropertyPress: function (oEvent) {
+      var sPropertyName = oEvent.getSource().data().PropertyName;
+      if (!sPropertyName) {
+        // TODO: add error message
+        $.sap.log.error('');
+        return;
+      }
+      this.goLowerLevel(sPropertyName);
+      this._loadPropertiesAtDepth();
+      this._setPropertyTitle(this.getCurrentPropertyPath(true));
+      return;
+    },
+
+    onModelRefresh: function (oEvent) {
+      var oModelHelper = this.getModelHelper();
+      oModelHelper.getModel(this.getCurrentModelName()).refresh(true);
+    },
+
+    onClosePopover: function (oEvent) {
+      this.getPopoverControl().close();
     },
 
     /**
@@ -222,9 +266,13 @@ sap.ui.define([
       var sModelName = this.getCurrentModelName();
       var oModelHelper = this.getModelHelper();
       var oModel = oModelHelper.getModel(sModelName);
-      var oModelData = oModel.getProperty('/');
+      var oModelData = oModel.getProperty(this.getCurrentPropertyPath(false));
       var aProperties = this._getControlArray(oModelData);
+
+      var oPropertiesPage = Fragment.byId(this.getFragmentId(), 'properties');
+      oPropertiesPage.focus();
       oPropertyList.destroyItems();
+
       for (var i = 0; i < aProperties.length; i++) {
         oPropertyList.addItem(aProperties[i]);
       }
@@ -249,7 +297,9 @@ sap.ui.define([
         if (oModelData.hasOwnProperty(sPropertyName)) {
           var oProperty = oModelData[sPropertyName];
           var oListItem = this._createListItemFromProperty(sPropertyName, oProperty);
-          aCurrentDepthProperties.push(oListItem);
+          if (oListItem) {
+            aCurrentDepthProperties.push(oListItem);
+          }
         }
       }
       return aCurrentDepthProperties;
@@ -257,47 +307,58 @@ sap.ui.define([
 
     _createListItemFromProperty: function (sPropertyName, oProperty) {
       var sModelName = this.getCurrentModelName();
-      var sPropertyPath = this.getCurrentPropertyPath();
+      var sPropertyPath = this.getCurrentPropertyPath(true);
       var oControl = null;
-      var sListItemType = null;
+      var sListItemType = 'Inactive';
+      var fnOnPress = function () {};
+
+      // trim trailing slash
+      if (sPropertyPath.substring(sPropertyPath.length, sPropertyPath.length - 1) === '/') {
+        sPropertyPath = sPropertyPath.slice(0, -1);
+      }
+
       var sPropertyBindingPath = '{' + sPropertyPath + '/' + sPropertyName + '}';
 
       switch (typeof oProperty) {
         case "function":
           // do nothing ... 
-          break;
+          return null;
         case "object":
           // allow drill-down to next level
+          sListItemType = 'Navigation';
+          fnOnPress = this.onPropertyPress;
           break;
         case "number":
-          //sListItemType = 'Active';
           oControl = new sap.m.Input({
             value: sPropertyBindingPath,
             type: 'Number'
           });
           break;
         case "string":
-          //sListItemType = 'Active';
           oControl = new sap.m.Input({
             value: sPropertyBindingPath
           });
           break;
         case "boolean":
           oControl = new sap.m.Switch({
-            state: sPropertyBindingPath
+            state: sPropertyBindingPath,
+            customTextOn: ' ',
+            customTextOff: ' '
           });
           break;
         default:
           var sPropertyType = typeof oProperty;
-          debugger;
           // do nothing ... not handled
           break;
       }
 
       var oListItem = new InputListItem({
         label: sPropertyName,
+        type: sListItemType,
+        press: fnOnPress.bind(this),
         content: (oControl) ? [oControl] : []
       });
+      oListItem.data('PropertyName', sPropertyName);
 
       return oListItem;
     }
